d8b153197ede6c5b1ac637e71f1be64c012f2193
Sven Schulze <47818658+schulze117@users.noreply.github.com>
2026-01-19 15:01:27 +0100

    Update proxy settings and sleep duration in tests

--- scrape_test.py
+++ scrape_test.py
@@ -1,18 +1,18 @@
 from seleniumbase import SB
 
 with SB(uc=True, 
-        # proxy=PROXY_URL, 
-        xvfb=True,
-        headless=True,
+        proxy="http://10.156.0.22:8888", 
+        # xvfb=True,
+        headless=False,
         locale="en", 
         incognito=True, 
         block_images=True,
         ) as sb:
     
-    url = "https://www.immowelt.de/expose/8c1cd9b8-c2be-4fe0-9f12-007137aa2836"
+    url = "https://www.immowelt.de/expose/f6dd6b66-3ec3-4682-80f8-25533496f226"
     sb.activate_cdp_mode(url)
 
-    sb.sleep(4)
+    sb.sleep(6)
 
     html = sb.get_page_source()
     print("HTML Length:", len(html))
