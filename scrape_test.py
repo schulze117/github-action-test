757b06c18f26284ae9f0f1bba2974292497494ae
Sven Schulze <47818658+schulze117@users.noreply.github.com>
2026-01-20 17:43:06 +0100

    Disable IP fetching in scrape_test.py
    Comment out IP fetching code in scrape_test.py

--- scrape_test.py
+++ scrape_test.py
@@ -9,12 +9,11 @@ with SB(uc=True,
         block_images=True,
         ) as sb:
 
-    url = "https://api.ipify.org/"
-    sb.activate_cdp_mode(url)
-    sb.sleep(2)
-
-    html = sb.get_page_source()
-    print("IP:", html)
+    # url = "https://api.ipify.org/"
+    # sb.activate_cdp_mode(url)
+    # sb.sleep(2)
+    # html = sb.get_page_source()
+    # print("IP:", html)
                 
     url = "https://www.immowelt.de/expose/f6dd6b66-3ec3-4682-80f8-25533496f226"
     sb.activate_cdp_mode(url)
